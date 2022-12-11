82283c6aa1f9da41f8fd0e6f1f125a11e97f242e
husam <husam999h@gmail.com>
2022-12-11 09:54:26 +0300

    finished with hard coded values

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
-import { watch } from "node:fs/promises"
+import { watch, stat } from "node:fs/promises"
 import { fileURLToPath } from "node:url";
-import { dirname } from "node:path";
+import { dirname, resolve } from "node:path";
+import { createReadStream, createWriteStream } from "node:fs";
+import { spawn } from "node:child_process";
 
 const currenDir = dirname(fileURLToPath(import.meta.url))
+const encryptionKey = 'test'
 const ac = new AbortController()
 
 process.on('SIGINT', () => {
@@ -14,8 +17,55 @@ try {
 	const watcher = watch(currenDir, { signal: ac.signal });
 	console.log(`watching dir: ${currenDir}`)
 
-	for await (const event of watcher)
+	for await (const event of watcher) {
 		console.log(event);
+
+		if (event.filename.endsWith('.xor') || event.eventType === "change") {
+			continue
+		}
+
+		const fileName = resolve(currenDir, event.filename)
+
+		let fileStat
+		try {
+			fileStat = await stat(fileName)
+		} catch (error: any) {
+			if (error.code === "ENOENT")
+				continue
+			else
+				throw error
+		}
+
+		if (fileStat.isDirectory()) {
+			console.error(`${fileName}: Cannot watch events on nested directories`)
+			continue
+		}
+
+		if (!fileStat.isFile()) {
+			console.error(`${fileName}: Unknown file type`)
+			continue
+		}
+
+		console.log(`spawning child process to encrypt file ${fileName}`)
+
+		const xorProcessor = spawn('xor', [encryptionKey], {
+			stdio: [
+				'pipe',
+				'pipe',
+				'inherit',
+			] 
+		})
+
+		createReadStream(fileName).pipe(xorProcessor.stdin)
+		xorProcessor.stdout.pipe(createWriteStream(`${fileName}.xor`))
+
+		xorProcessor.on('close', () => {
+			// remove original file
+			spawn('rm', [fileName])
+		})
+
+
+	}
 
 } catch (err: any) {
 	if (err?.name === 'AbortError')
